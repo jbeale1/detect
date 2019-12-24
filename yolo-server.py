0d24a71ef1b5217c158b843814a2edaf2aa60a8a
John Beale <jbeale1@users.noreply.github.com>
2019-12-24 13:08:55 -0800

    add output to logfile

--- yolo-server.py
+++ yolo-server.py
@@ -7,30 +7,42 @@
 
 import subprocess 
 import threading
+import datetime         # datetime.now() for log file header
 from time import sleep
 import os, fcntl    # iterate over files
 import sys          # sys.exit()
 import cv2
 # ================================================================
-
 inpath = "/home/pi/yinput/"    # directory of images to process
 outpath = "/home/pi/youtput/"  # detected crops saved here
+logfname = "/home/pi/youtput/Detlog.txt"
+
+# ================================================================
+
+# write detections for this image into log file
+def putLog(imgname, dets):
+  if (imgname != ''):
+    logfile.write("%s, " % imgname)
+    if len(dets) > 0:
+      for (cat,p) in dets:
+        logfile.write("%s:%s," % (cat,p))
+    else:
+      logfile.write("#Nothing:0%")
+    logfile.write("\n")
+
+# ================================================================
 
 folder = os.fsencode(inpath)
-
 filenames = []
-
 for file in os.listdir(folder):
     filename = os.fsdecode(file)
     if filename.endswith( ('.jpg', '.png') ): # get the images
         filenames.append(filename)
 
-filenames.sort() # os.listdir gives them in random order
+filenames.sort() # os.listdir gives them in random order, so fix that
 
-print("First file: %s\n" % filenames[0])
 
 # start darknet process. Note: detections below 50% are more often wrong than right
-
 """
 yolo_proc = subprocess.Popen(["./darknet",
                    "detect",
@@ -47,27 +59,41 @@ yolo_proc = subprocess.Popen(["./darknet",
                    "-thresh","0.5"],
                    stdin = subprocess.PIPE, stdout = subprocess.PIPE)
 
+
 fcntl.fcntl(yolo_proc.stdout.fileno(), fcntl.F_SETFL, os.O_NONBLOCK)
 # ===========================================
 
 i = 0
 rl = 0
+fname = ''  # start with null input filename
+dets = []   # start with no detections
 
 print("Started Yolo\n")
+print("First file: %s\n" % filenames[0])
+
+logfile = open(logfname,"a+")
+logfile.write("Starting Yolo-v3 : %s\n" % str(datetime.datetime.now()) )  # append to log file, or create it if needed
+
 
 while True:
         yline = yolo_proc.stdout.readline()  # get one line from process as string
-        scount = len(yline)
+        yout = yline.decode('ascii').strip()
+        scount = len(yout)
         sleep(0.5)
         if scount>0:  # any non-whitespace in output line?
           rl += 1                # increment count of returned lines
-          yout = yline.decode('ascii')
 
-          if "Enter Image Path" in yout:
+          if "Enter Image Path" in yout:  # darknet ready for new image
+            putLog(fname, dets)         # save detections from previous image (if any)
+
             try:
               fpath = inpath + filenames[i]
               fpathcr = fpath + '\n'
+              fname = os.path.basename(fpath.strip())  # base filename
+              dets.clear()
             except:
+              putLog(fname, dets)         # save detections from previous image (if any)
+              logfile.close()
               print("All done: files = %d\n" % i)  # exit when finished with all files
               sys.exit()
 
@@ -78,8 +104,9 @@ while True:
             i += 1
           elif "Predicted in" in yout:
             print(yout)
+          elif "Working on" in yout:
+            print(yout)
           else:
-            fname = os.path.basename(fpath.strip())  # base filename
             fbase = os.path.splitext(fname)[0]  # pathname without '.jpg' and '\n'
             fout = outpath + fbase
             # yout example string: 'person: 95.0% 1: Crop: 268x550+552+438'
@@ -89,6 +116,7 @@ while True:
             detidx = yout.split(':')[1].split(' ')[-1]  # index of detection within this image
             detconf = yout.split(':')[1].split(' ')[-2]   # confidence %
             print("detected class = %s : %s" % (detclass,detconf) ) # display detected class (maybe more than one word?)
+            dets.append([detclass,detconf])   # save this detection in list for log
             gspec = yout.split(':')[-1].strip()
             cstr = "convert " + fpath + " -crop " + gspec + ' ' + fout + '_' + detclass + '_' + detidx + '.jpg'
             # print(cstr)  # full command string
